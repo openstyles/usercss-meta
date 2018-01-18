7c74de3deae2245f637a34402450f44aee01cbb9
eight <eight04@gmail.com>
2018-01-18 19:39:48 +0800

    Test: rewrite, use actual metadata string

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -1,15 +1,309 @@
-import test from 'ava';
-import usercss from '../index';
-import {default as styles} from './styles';
+/* eslint dot-notation: 0 */
 
-/* Test buildMeta parser only */
-styles.forEach(s => {
-  test(s.info, t => {
-    if (s.error) {
-      const error = t.throws(() => usercss.parseMeta(s.input));
-      t.is(error.message, s.error);
-    } else {
-      t.deepEqual(usercss.parseMeta(s.input), s.output);
-    }
+import test from 'ava';
+import {createParser} from '../lib/usercss';
+// import {default as styles} from './styles';
+const parser = createParser();
+const looseParser = createParser({mandatoryKeys: []});
+
+test('Default template', t => {
+  const meta = `
+/* ==UserStyle==
+@name        test
+@namespace   github.com/openstyles/stylus
+@version     0.1.0
+@description my userstyle
+@author      Me
+==/UserStyle== */`.trim();
+
+  t.deepEqual(parser.parse(meta), {
+    name: 'test',
+    namespace: 'github.com/openstyles/stylus',
+    version: '0.1.0',
+    description: 'my userstyle',
+    author: 'Me',
+    vars: {}
   });
+});
+
+test('Missing metadata @name', t => {
+  const meta = `
+/* ==UserStyle==
+@namespace   github.com/openstyles/stylus
+@version     0.1.0
+==/UserStyle== */`.trim();
+
+  const error = t.throws(() => parser.parse(meta));
+  t.is(error.message, 'Missing metadata @name');
+});
+
+test('Missing metadata @namespace', t => {
+  const meta = `
+/* ==UserStyle==
+@name        test
+@version     0.1.0
+==/UserStyle== */`.trim();
+
+  const error = t.throws(() => parser.parse(meta));
+  t.is(error.message, 'Missing metadata @namespace');
+});
+
+test('Missing metadata @version', t => {
+  const meta = `
+/* ==UserStyle==
+@name        test
+@namespace   github.com/openstyles/stylus
+==/UserStyle== */`.trim();
+
+  const error = t.throws(() => parser.parse(meta));
+  t.is(error.message, 'Missing metadata @version');
+});
+
+test('Invalid version', t => {
+  const meta = `
+/* ==UserStyle==
+@version 0.1.x
+==/UserStyle== */`.trim();
+
+  const error = t.throws(() => looseParser.parse(meta));
+  t.is(error.message, '0.1.x is not a valid version');
+});
+
+test('Normalize version', t => {
+  const meta = `
+/* ==UserStyle==
+@version v0.1.0
+==/UserStyle== */`.trim();
+
+  t.is(looseParser.parse(meta).version, '0.1.0');
+});
+
+test('basic @var color', t => {
+  const meta = `
+/* ==UserStyle==
+@var color font-color 'Font-color' #123456
+==/UserStyle== */`.trim();
+
+  const va = looseParser.parse(meta).vars['font-color'];
+  t.is(va.type, 'color');
+  t.is(va.label, 'Font-color');
+  t.is(va.default, '#123456');
+});
+
+test('basic @var select', t => {
+  const meta = `
+/* ==UserStyle==
+@var select nav-pos "Navbar pos" {
+  "Top": "top",
+  "Bottom": "bottom",
+  "Right": "right",
+  "Left": "left"
+}
+==/UserStyle== */`.trim();
+
+  const va = looseParser.parse(meta).vars['nav-pos'];
+  t.is(va.type, 'select');
+  t.is(va.label, 'Navbar pos');
+  t.is(va.default, 'Top');
+  t.deepEqual(va.options, [
+    {
+      name: 'Top',
+      label: 'Top',
+      value: 'top'
+    },
+    {
+      name: 'Bottom',
+      label: 'Bottom',
+      value: 'bottom'
+    },
+    {
+      name: 'Right',
+      label: 'Right',
+      value: 'right'
+    },
+    {
+      name: 'Left',
+      label: 'Left',
+      value: 'left'
+    },
+  ]);
+});
+
+test('basic @var select with built-in label', t => {
+  const meta = `
+/* ==UserStyle==
+@var select nav-pos Navbar {
+  'near_black:Near Black': '#111111',
+  'near_white:Near White': '#eeeeee'
+}
+==/UserStyle== */`.trim();
+
+  const va = looseParser.parse(meta).vars['nav-pos'];
+  t.is(va.type, 'select');
+  t.is(va.label, 'Navbar');
+  t.is(va.default, 'near_black');
+  t.deepEqual(va.options, [
+    {
+      name: 'near_black',
+      label: 'Near Black',
+      value: '#111111'
+    },
+    {
+      name: 'near_white',
+      label: 'Near White',
+      value: '#eeeeee'
+    }
+  ]);
+});
+
+test('basic @var select set as an array', t => {
+  const meta = `
+/* ==UserStyle==
+@var select theme "Theme" ['dark', 'light']
+==/UserStyle== */`.trim();
+
+  const va = looseParser.parse(meta).vars['theme'];
+  t.is(va.type, 'select');
+  t.is(va.label, 'Theme');
+  t.is(va.default, 'dark');
+  t.deepEqual(va.options, [
+    {
+      name: 'dark',
+      label: 'dark',
+      value: 'dark'
+    },
+    {
+      name: 'light',
+      label: 'light',
+      value: 'light'
+    }
+  ]);
+});
+
+test('basic @var select set as an array with built-in label', t => {
+  const meta = `
+/* ==UserStyle==
+@var select theme "Theme" ['dark:Dark theme', 'light:Light theme']
+==/UserStyle== */`.trim();
+
+  const va = looseParser.parse(meta).vars['theme'];
+  t.is(va.type, 'select');
+  t.is(va.label, 'Theme');
+  t.is(va.default, 'dark');
+  t.deepEqual(va.options, [
+    {
+      name: 'dark',
+      label: 'Dark theme',
+      value: 'dark'
+    },
+    {
+      name: 'light',
+      label: 'Light theme',
+      value: 'light'
+    }
+  ]);
+});
+
+test('basic @var checkbox', t => {
+  const meta = `
+/* ==UserStyle==
+@var checkbox affix "Set affixed" 1
+==/UserStyle== */`.trim();
+
+  const va = looseParser.parse(meta).vars['affix'];
+  t.is(va.type, 'checkbox');
+  t.is(va.label, 'Set affixed');
+  t.is(va.default, '1');
+});
+
+test('basic @var text', t => {
+  const meta = `
+/* ==UserStyle==
+@var text height "Set height" 10px
+==/UserStyle== */`.trim();
+
+  const va = looseParser.parse(meta).vars['height'];
+  t.is(va.type, 'text');
+  t.is(va.label, 'Set height');
+  t.is(va.default, '10px');
+});
+
+test('basic @advanced text', t => {
+  const meta = `
+/* ==UserStyle==
+@advanced text height "Set height" "10px"
+==/UserStyle== */`.trim();
+
+  const va = looseParser.parse(meta).vars['height'];
+  t.is(va.type, 'text');
+  t.is(va.label, 'Set height');
+  t.is(va.default, '10px');
+});
+
+test('basic @advanced color', t => {
+  const meta = `
+/* ==UserStyle==
+@advanced color font-color "Font color" #ffffff
+==/UserStyle== */`.trim();
+
+  const va = looseParser.parse(meta).vars['font-color'];
+  t.is(va.type, 'color');
+  t.is(va.label, 'Font color');
+  t.is(va.default, '#ffffff');
+});
+
+test('basic @advanced image', t => {
+  const meta = `
+/* ==UserStyle==
+@advanced image background "Page background" {
+  bg_1 "Background 1" "http://example.com/example.jpg"
+  bg_2 "Background 2" "http://example.com/photo.php?id=_A_IMAGE_ID_"
+}
+==/UserStyle== */`.trim();
+
+  const va = looseParser.parse(meta).vars['background'];
+  t.is(va.type, 'image');
+  t.is(va.label, 'Page background');
+  t.is(va.default, 'bg_1');
+  t.deepEqual(va.options, [
+    {
+      name: 'bg_1',
+      label: 'Background 1',
+      value: 'http://example.com/example.jpg'
+    },
+    {
+      name: 'bg_2',
+      label: 'Background 2',
+      value: 'http://example.com/photo.php?id=_A_IMAGE_ID_'
+    }
+  ]);
+});
+
+test('basic @advanced dropdown', t => {
+  const meta = `
+/* ==UserStyle==
+@advanced dropdown browser "Your browser" {
+  fx "Firefox" <<<EOT
+background-color: red; EOT;
+  cr "Chrome" <<<EOT
+background-color: green; EOT;
+}
+==/UserStyle== */`.trim();
+
+  const va = looseParser.parse(meta).vars['browser'];
+  t.is(va.type, 'dropdown');
+  t.is(va.label, 'Your browser');
+  t.is(va.default, 'fx');
+  t.deepEqual(va.options, [
+    {
+      name: 'fx',
+      label: 'Firefox',
+      value: 'background-color: red;'
+    },
+    {
+      name: 'cr',
+      label: 'Chrome',
+      value: 'background-color: green;'
+    }
+  ]);
 });
