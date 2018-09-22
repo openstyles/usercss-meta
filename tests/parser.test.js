afc662177b9f726ddb410c8759b0cd0347c1d4b5
eight <eight04@gmail.com>
2018-09-22 16:25:21 +0800

    WIP: add more cases

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -2,7 +2,7 @@
 
 import fs from 'fs';
 import test from 'ava';
-import {createParser, util, parse, ParseError} from '..';
+import {createParser, util, parse, ParseError, stringify} from '..';
 
 const looseParser = createParser({mandatoryKeys: []});
 
@@ -38,7 +38,7 @@ for (const dir of fs.readdirSync(`${__dirname}/cases`)) {
     const error = tryReadJSON(`${__dirname}/cases/${dir}/error.json`);
 
     function run() {
-      return createParser({mandatoryKeys: []}).parse(text);
+      return parse(text, {mandatoryKeys: []});
     }
 
     if (error) {
@@ -54,297 +54,22 @@ for (const dir of fs.readdirSync(`${__dirname}/cases`)) {
 
     const result = run();
     t.deepEqual(result.metadata, metadata);
+
+    const {metadata: newMetadata} = parse(stringify(metadata), {mandatoryKeys: []});
+    t.deepEqual(newMetadata, metadata);
   });
 }
 
 test('mandatoryKeys', t => {
-  // TBD
-});
-
-test('basic @var select', t => {
-  const meta = `
-    /* ==UserStyle==
-    @var select nav-pos "Navbar pos" {
-      "Top": "top",
-      "Bottom": "bottom",
-      "Right": "right",
-      "Left": "left"
-    }
-    ==/UserStyle== */
-  `;
-
-  const va = looseParser.parse(meta).metadata.vars['nav-pos'];
-  t.is(va.type, 'select');
-  t.is(va.label, 'Navbar pos');
-  t.is(va.default, 'Top');
-  t.deepEqual(va.options, [
-    {
-      name: 'Top',
-      label: 'Top',
-      value: 'top'
-    },
-    {
-      name: 'Bottom',
-      label: 'Bottom',
-      value: 'bottom'
-    },
-    {
-      name: 'Right',
-      label: 'Right',
-      value: 'right'
-    },
-    {
-      name: 'Left',
-      label: 'Left',
-      value: 'left'
-    },
-  ]);
-});
-
-test('basic @var select error', t => {
-  const {text, raw} = extractRange(`
-    /* ==UserStyle==
-    @var select nav-pos "Navbar pos" |{}
-    ==/UserStyle== */
-  `);
-
-  const error = t.throws(() => looseParser.parse(text));
-  t.is(error.message, 'Option list is empty');
-  t.is(drawRange(text, error.index), raw);
-});
-
-test('basic @var select with built-in label', t => {
-  const meta = `
-    /* ==UserStyle==
-    @var select nav-pos Navbar {
-      'near_black:Near Black': '#111111',
-      'near_white:Near White': '#eeeeee'
-    }
-    ==/UserStyle== */
-  `;
-
-  const va = looseParser.parse(meta).metadata.vars['nav-pos'];
-  t.is(va.type, 'select');
-  t.is(va.label, 'Navbar');
-  t.is(va.default, 'near_black');
-  t.deepEqual(va.options, [
-    {
-      name: 'near_black',
-      label: 'Near Black',
-      value: '#111111'
-    },
-    {
-      name: 'near_white',
-      label: 'Near White',
-      value: '#eeeeee'
-    }
-  ]);
-});
-
-test('basic @var select set as an array', t => {
-  const meta = `
-    /* ==UserStyle==
-    @var select theme "Theme" ['dark', 'light']
-    ==/UserStyle== */
-  `;
-
-  const va = looseParser.parse(meta).metadata.vars['theme'];
-  t.is(va.type, 'select');
-  t.is(va.label, 'Theme');
-  t.is(va.default, 'dark');
-  t.deepEqual(va.options, [
-    {
-      name: 'dark',
-      label: 'dark',
-      value: 'dark'
-    },
-    {
-      name: 'light',
-      label: 'light',
-      value: 'light'
-    }
-  ]);
-});
-
-test('basic @var select set as an array with built-in label', t => {
-  const meta = `
-    /* ==UserStyle==
-    @var select theme "Theme" ['dark:Dark theme', 'light:Light theme']
-    ==/UserStyle== */
-  `;
-
-  const va = looseParser.parse(meta).metadata.vars['theme'];
-  t.is(va.type, 'select');
-  t.is(va.label, 'Theme');
-  t.is(va.default, 'dark');
-  t.deepEqual(va.options, [
-    {
-      name: 'dark',
-      label: 'Dark theme',
-      value: 'dark'
-    },
-    {
-      name: 'light',
-      label: 'Light theme',
-      value: 'light'
-    }
-  ]);
-});
-
-test('basic @var select specify default', t => {
-  const meta = `
-    /* ==UserStyle==
-    @var select theme "Theme" ['dark', 'light*']
-    ==/UserStyle== */
-  `;
-
-  const va = looseParser.parse(meta).metadata.vars['theme'];
-  t.is(va.type, 'select');
-  t.is(va.label, 'Theme');
-  t.is(va.default, 'light');
-  t.deepEqual(va.options, [
-    {
-      name: 'dark',
-      label: 'dark',
-      value: 'dark'
-    },
-    {
-      name: 'light',
-      label: 'light',
-      value: 'light'
-    }
-  ]);
-});
-
-test('basic @var select specify default with label', t => {
-  const meta = `
-    /* ==UserStyle==
-    @var select theme "Theme" ['dark:Dark theme', 'light:Light theme*']
-    ==/UserStyle== */
-  `;
-
-  const va = looseParser.parse(meta).metadata.vars['theme'];
-  t.is(va.type, 'select');
-  t.is(va.label, 'Theme');
-  t.is(va.default, 'light');
-  t.deepEqual(va.options, [
-    {
-      name: 'dark',
-      label: 'Dark theme',
-      value: 'dark'
-    },
-    {
-      name: 'light',
-      label: 'Light theme',
-      value: 'light'
-    }
-  ]);
-});
-
-test('basic @var select specify default with object', t => {
-  const meta = `
-    /* ==UserStyle==
-    @var select theme "Theme" {
-      'dark:Dark theme': 'black',
-      'light:Light theme*': 'white'
-    }
-    ==/UserStyle== */
-  `;
-
-  const va = looseParser.parse(meta).metadata.vars['theme'];
-  t.is(va.type, 'select');
-  t.is(va.label, 'Theme');
-  t.is(va.default, 'light');
-  t.deepEqual(va.options, [
-    {
-      name: 'dark',
-      label: 'Dark theme',
-      value: 'black'
-    },
-    {
-      name: 'light',
-      label: 'Light theme',
-      value: 'white'
-    }
-  ]);
-});
-
-test('basic @var select specify default with object without name', t => {
-  const meta = `
-    /* ==UserStyle==
-    @var select theme "Theme" {
-      'Dark theme': 'black',
-      'Light theme*': 'white'
-    }
-    ==/UserStyle== */
-  `;
-
-  const va = looseParser.parse(meta).metadata.vars['theme'];
-  t.is(va.type, 'select');
-  t.is(va.label, 'Theme');
-  t.is(va.default, 'Light theme');
-  t.deepEqual(va.options, [
-    {
-      name: 'Dark theme',
-      label: 'Dark theme',
-      value: 'black'
-    },
-    {
-      name: 'Light theme',
-      label: 'Light theme',
-      value: 'white'
-    }
-  ]);
-});
-
-test('basic @var select specify multiple defaults', t => {
-  const {text, raw} = extractRange(`
-    /* ==UserStyle==
-    @var select theme "Theme" |['dark*', 'light*']
-    ==/UserStyle== */
-  `);
-
-  const err = t.throws(() => looseParser.parse(text));
-  t.is(err.code, 'invalidSelectMultipleDefaults');
-  t.is(drawRange(text, err.index), raw);
-});
-
-test('basic @var checkbox', t => {
-  const meta = `
-    /* ==UserStyle==
-    @var checkbox affix "Set affixed" 1
-    ==/UserStyle== */
-  `;
-
-  const va = looseParser.parse(meta).metadata.vars['affix'];
-  t.is(va.type, 'checkbox');
-  t.is(va.label, 'Set affixed');
-  t.is(va.default, '1');
-});
-
-test('basic @var checkbox error', t => {
-  const {text, raw} = extractRange(`
-    /* ==UserStyle==
-    @var checkbox affix "Set affixed" |2
-    ==/UserStyle== */
-  `);
-
-  const error = t.throws(() => looseParser.parse(text));
-  t.is(error.message, 'value must be 0 or 1');
-  t.is(drawRange(text, error.index), raw);
-});
-
-test('basic @var text', t => {
-  const meta = `
-    /* ==UserStyle==
-    @var text height "Set height" 10px
-    ==/UserStyle== */
-  `;
-
-  const va = looseParser.parse(meta).metadata.vars['height'];
-  t.is(va.type, 'text');
-  t.is(va.label, 'Set height');
-  t.is(va.default, '10px');
+  const err = t.throws(() => {
+    parse(`
+      /* ==UserStyle==
+      @name foo
+      ==/UserStyle== */
+    `);
+  });
+  t.is(err.code, 'missingMandatory');
+  t.deepEqual(err.args, ['namespace', 'version']);
 });
 
 test('@var number', t => {
