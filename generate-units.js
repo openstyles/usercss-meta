6d0d6f9351cb5673fe91b693c1792066abbebf71
eight <eight04@gmail.com>
2018-10-02 23:57:35 +0800

    Add: invalidRangeUnits error (#51)
    * Update readme
    
    * Add: invalidRangeUnits error
    
    * Test: invalid units
    
    * Add: generate units script

---
+++ generate-units.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+fetch('https://drafts.csswg.org/css-values-4/')
+  .then(r => r.text())
+  .then(text => {
+    const $ = cheerio.load(text);
+    const units = [];
+    $('.heading').each((i, heading) => {
+      const level = $(heading).data('level');
+      if (!level || !/^[67]\./.test(level)) {
+        return;
+      }
+      $(heading).find('.css').each((i, css) => {
+        const unit = $(css).text();
+        if (unit[0] !== '<') {
+          units.push(unit);
+        }
+      });
+    });
+    units.push('%');
+    console.log(`module.exports = [${units.map(u => `'${u}'`).join(', ')}];`);
+  });
