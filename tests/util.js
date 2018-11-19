642eb09ac0564734a5304efb6dbda88dd5b9d02b
eight <eight04@gmail.com>
2018-11-19 20:39:50 +0800

    Fix: move tryReadJSON to util

--- tests/util.js
+++ tests/util.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 function extractRange(text) {
   const index = text.indexOf('|');
   return {
@@ -11,5 +13,15 @@ function drawRange(text, index) {
   return text.slice(0, index) + '|' + text.slice(index);
 }
 
+function tryReadJSON(path) {
+  let text = null;
+  try {
+    text = fs.readFileSync(path, 'utf8');
+  } catch (err) {
+    return;
+  }
+  return JSON.parse(text);
+}
+
 // https://github.com/avajs/ava/issues/1319
-module.exports = {extractRange, drawRange};
+module.exports = {extractRange, drawRange, tryReadJSON};
