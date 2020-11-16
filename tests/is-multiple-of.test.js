0c0b83102780e0335a8d1306249e86727b047d44
tophf <tophf@gmx.com>
2020-11-16 12:35:23 +0300

    add test

---
+++ tests/is-multiple-of.test.js
@@ -0,0 +1,10 @@
+import test from 'ava';
+import {isMultipleOf} from '../lib/parse';
+
+test('isMultipleOf', t => {
+  t.truthy(isMultipleOf(0.3, 0.1));
+  t.truthy(isMultipleOf(0.07, 0.01));
+  t.truthy(isMultipleOf(0.777777777777777777, 0.111111111111111111)); // exceeds 15-digit precision limit
+  t.truthy(isMultipleOf(12345678901234567890, 2)); // exceeds 15-digit precision limit
+  t.falsy(isMultipleOf(7, 0.3));
+});
